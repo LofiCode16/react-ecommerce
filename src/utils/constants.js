3d8230574dddb436a584b8735f78c345d38aaf39
Diego perez <diegopr178@gmail.com>
2021-06-01 17:22:30 -0400

    UPDATE: readme

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const URL_API_PRODUCTS = process.env.REACT_APP_API_PRODUCTS;
-export const BASE_PATH = process.env.REACT_APP_BASE_URL;
-export const STORAGE_PRODUCTS_CART = process.env.REACT_APP_STORAGE;
+export const URL_API_PRODUCTS = process.env.REACT_APP_API_PRODUCTS; // endpoint para obtener los productos
+export const BASE_PATH = process.env.REACT_APP_BASE_URL; // dominio de la app
+export const STORAGE_PRODUCTS_CART = process.env.REACT_APP_STORAGE; // nombre del storage
